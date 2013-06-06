ac1315e5de5ab929f509d79ae0f6167abe850f8d
Cesar Torres [Cantinflas] <ctorres7@stanford.edu>
2013-06-05 21:40:41 -0700

    opt mod and demod, not lpf

--- milestone3/common_txrx_mil3.py
+++ milestone3/common_txrx_mil3.py
@@ -1,37 +1,63 @@
 import numpy
 import math as m
 import operator
+import time
 
+class Timer:    
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        self.interval = self.end - self.start
 # Methods common to both the transmitter and receiver
 def modulate(fc, samplerate, samples):
   '''
   A modulator that multiplies samples with a local carrier 
   of frequency fc, sampled at samplerate
   '''
-  print "MODULATE", fc, samplerate, len(samples)
-  y = []
-  fs = samplerate
+  #print "MODULATE", fc, samplerate, len(samples)
+  try:
+    with Timer() as t:
+        y = []
+        fs = samplerate
+        omega_c = 2 * m.pi * fc / fs
 
-  #carrier = cos(2 * math.pi * fc / fs * n)
-  omega_c = 2 * m.pi * fc / fs
-  for n in range(0, len(samples)):
-    y.append(samples[n] * m.cos(omega_c * n))
+        temp = range(0, len(samples))
+        carrier = [m.cos(omega_c * n) for n in temp]
+        y = numpy.multiply(samples, carrier)
+  finally:
+    print('Modulation took %.03f sec.' % t.interval)
+
   return y
 
 def demodulate(fc, samplerate, samples):
   '''
   A demodulator that performs quadrature demodulation
   '''
-  fs = samplerate
-  omega_c = 2 * m.pi * fc / fs
-  w = []
+  try:
+    with Timer() as t:
+      fs = samplerate
+      omega_c = 2 * m.pi * fc / fs
+      
 
-  for n in range(0, len(samples)):
-    w.append(samples[n] * numpy.power(m.e, 1j * omega_c * n))
+      temp = range(0, len(samples))
+      omega = [1j * omega_c * n for n in temp]
+      carrier = [numpy.power(m.e, om) for om in omega]
+      w = numpy.multiply(samples, carrier)
 
-  demod_samples = lpfilter(w, omega_c / 2.0)
+  finally:
+    print('Modulation took %.03f sec.' % t.interval)
+  
 
 
+  try:
+    with Timer() as t:
+      demod_samples = lpfilter(w, omega_c / 2.0)
+  finally:
+    print('LPF took %.03f sec.' % t.interval)
+  
   return [numpy.linalg.norm(n) for n in demod_samples]
 
 def lpfilter(samples_in, omega_cut):
@@ -43,6 +69,7 @@ def lpfilter(samples_in, omega_cut):
   h = []
   dmod = []
 
+  
   for n in range(-L, L + 1):
     if n == 0:
       h.append(omega_cut / m.pi)
@@ -52,18 +79,15 @@ def lpfilter(samples_in, omega_cut):
 
   #Demodulating samples
   for n in range(0, len(samples_in)):
+
     sl = 0 if n - L < 0 else n - L
     su = len(samples_in) - 1 if n + L > len(samples_in) - 1 else n + L + 1
+    
     hl = 0 if n - L >= 0 else - (n - L)
     hu = len(h) if n + L <= len(samples_in) - 1 else len(h) + ((len(samples_in) - 1) -  (n + L + 1)) 
     sum = 0
-    #if not (hl == 0 and hu == 101):
-    #  print sl, su, len(samples_in[sl: su])
-    # print hl, hu, len(h[hl:hu])
-    
     s_in = samples_in[sl:su]
     h_in = h[hl:hu]
-    #print len(s_in), len(h_in)
     for i in range(0, len(h_in)):
       sum += s_in[i] * h_in[i]
     dmod.append(sum)
